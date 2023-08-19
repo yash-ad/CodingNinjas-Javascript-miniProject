3c160d110a2d4bd3af8a41c3a833125ccb9f22e9
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-08-19 12:25:34 +0530

    Added new reset button for reset functionality

---
+++ Code/Script.js
@@ -0,0 +1,82 @@
+//This JavaScript code is designed to create a counter that starts from 0 and increases up to a user-defined
+//number between 1 and 9.
+//The counter is displayed with a smooth animation.
+
+
+//Code below:-
+let countInterval;
+//countInterval: This is a variable that will store the interval for the counter.
+
+//<button onclick="startCounter()">Start Counter</button>
+function startCounter()
+//This function is triggered when the "Start Counter" button is clicked.
+//It manages the counter animation.
+ {
+//The value entered by the user in the input field.
+let number = parseInt(document.getElementById("number").value);
+
+//The HTML element that displays the current number. 
+let currentNum = document.querySelector(".counter .current");
+
+//The HTML element that displays the next number.
+  let nextNum = document.querySelector(".counter .next");
+
+//count: Keeps track of the current count value.
+  let count = 0;
+
+
+//This function resets the counter numbers.
+//It sets the current number to 0 and the next number to 1.
+  resetNumbers(currentNum, nextNum);
+  if (number < 1 || number > 9) {
+    clearInterval(countInterval);
+    return;
+  }
+
+//Clears the previous interval that was running
+  clearInterval(countInterval);
+  countInterval = setInterval(function () {
+    if (count === number) {
+      clearInterval(countInterval);
+      if(count === 9)
+      {
+
+alert("Times up and Refresh the page!")
+      }
+      return;
+    }
+//This function increases the counter by 1 and updates the displayed numbers with a smooth animation.    
+    increaseCount(currentNum, nextNum);
+    count++;
+  }, 1000);
+}
+
+function resetNumbers(currentNo, nextNo, end) {
+  currentNo.innerHTML = 0;
+  nextNo.innerHTML = 1;
+}
+function increaseCount(currentNo, nextNo) {
+  nextNo.classList.add("animate");
+  currentNo.innerHTML = nextNo.innerHTML;
+  nextNo.classList.remove("animate");
+  nextNo.innerHTML = parseInt(nextNo.innerHTML) + 1;
+}
+
+//<button onclick="resetCounter()">Reset Counter</button>
+//Reset button function
+function resetCounter()
+{
+let currentNum = document.querySelector(".counter .current");
+let nextNum = document.querySelector(".counter .next");
+clearInterval(countInterval);
+resetNumbers(currentNum,nextNum);
+resetInput();
+}
+
+//<input type="number" id="number" placeholder="Enter Number"/>
+//Input reset as well.
+function resetInput()
+{
+let inputReset = document.getElementById("number");
+inputReset.value = ""; //Reset the input field.
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# CodingNinjas-Javascript-miniProject
-Counter timer using HTML , CSS and JAVASCRIPT
+<!-- # CodingNinjas-Javascript-miniProject:-                                  ## Counter timer using HTML , CSS and JAVASCRIPT:-
+
+## Html code explanation:- -->
+<!-- 
+1. `<div id="counter-value">`: This is a container div for the counter value section. Inside this div, you have a paragraph (`<p>`) element that provides instructions to the user. It tells the user to enter a value between 1 and 9. Below the paragraph, there's another div (`<div id="input-range">`) that holds the input field and a button.
+
+2. `<input type="number" id="number" placeholder="Enter Number" />`: This is an input field of type "number." It has an `id` attribute set to "number," which can be used to reference this input field in JavaScript. The `placeholder` attribute provides a hint to the user that they should enter a number.
+
+3. `<button onclick="startCounter()">Start Counter</button>`: This is a button element that says "Start Counter." It has an `onclick` attribute, which means when the button is clicked, it will trigger a JavaScript function called `startCounter()`.
+
+4. `<div id="counter-container">`: This is another container div, this time for the counter display section. Inside this div, you have a single counter element (`<div class="counter" id="first">`) that displays the current and next values.
+
+5. `<p class="current"></p>`: This paragraph element with the class "current" will be used to display the current value of the counter.
+
+6. `<p class="next">1</p>`: This paragraph element with the class "next" contains the initial next value of the counter, which is set to  -->
